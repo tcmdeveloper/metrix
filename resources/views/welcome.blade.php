eb96501e9856d6b43f928bf55bcc1c375a324a52
Frank Jones <frankjones@Franks-MacBook-Pro.local>
2025-05-16 17:27:18 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+    <title>True Crime Metrix</title>
+  </head>
+  <body>
+    <div class="w-1/2 mx-auto pt-12">
+      <img src="{{asset('images/truecrimemetrix-logo.png')}}" alt="" name="" class="w-1/3 mx-auto my-16">
+      <h1 class="font-thunder text-center">
+        true crime metrix
+      </h1>
+      <a href="http://buymeacoffee.com/truecrimemetrix" target="_blank">
+        <img src="{{asset('images/bmc-button.png')}}" alt="" name="" class="w-1/2 mx-auto my-16 transition-all  duration-300 ease-in-out hover:-translate-y-4">
+    </div>
+  </body>
+</html>
